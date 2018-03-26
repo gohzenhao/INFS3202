989966aff1a37c7cbfbc80c56110b6aefec70f34
Yuliang Zhou <yuliangzhou7@gmail.com>
2018-03-26 22:18:22 +1000

    Auotload classes in libraries folder

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // App Root (dirname will go 1 directory up)
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost/infs3202project');
+    // Site Name
+    define('SITENAME', 'TheRecipesProject');
