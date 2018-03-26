a0253c29e52ab6e7898d9d72de7fbb7e194277cb
Yuliang Zhou <yuliangzhou7@gmail.com>
2018-03-26 21:56:02 +1000

    controllers can load views with parameters accessible within views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,15 @@
 <?php
-  class Pages {
+  class Pages extends Controller{
     public function __construct(){
 		echo 'Welcome';
     }
 
     public function index(){
-      
+		$data = ['title' => 'Welcome Peasants!'];
+    	$this->view('home/index', $data);
     }
 
-    public function about($id){
-      echo $id;
+    public function about(){
+		$this->view('home/about');
     }
   }
