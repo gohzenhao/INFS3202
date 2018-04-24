16d6cef4a666999c73e1ba27158b6a3b856fea8b
Yuliang Zhou <yuliangzhou7@gmail.com>
2018-04-24 18:00:19 +1000

    Added jquery-sortable library

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -4,6 +4,7 @@
     <!-- Bootstrap core JavaScript -->
     <script src="<?php echo URLROOT; ?>/vendor/jquery/jquery.min.js"></script>
     <script src="<?php echo URLROOT; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo URLROOT; ?>/vendor/jquery-sortable-min.js"></script>
 
     <script src="<?php echo URLROOT; ?>/js/main.js"></script>
 </body>
