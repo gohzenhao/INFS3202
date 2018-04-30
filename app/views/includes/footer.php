a7592fef805cabf97ec3f59e4546bf4376883587
Yuliang Zhou <yuliangzhou7@gmail.com>
2018-05-01 02:56:27 +1000

    Added basic footer

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,6 +1,21 @@
 
     </div>
 
+    <footer class="page-footer container-fluid py-3 bg-dark">
+        <div class="container text-center">
+            <div class="row w-80">
+                <h5 class="w-100 font-weight-bold text-white">Useful Links</h5>
+                <p class="col text-center"><a href="#!">Home</a></p>
+                <p class="col text-center"><a href="#!">About Us</a></p>
+                <p class="col text-center"><a href="#!">Cool new feature...</a></p>
+            </div>
+        </div>
+
+        <div class="footer-copyright text-center text-white">
+            © 2018 Copyright: TheRecipesProject
+        </div>
+    </footer>
+
     <!-- Bootstrap core JavaScript -->
     <script src="<?php echo URLROOT; ?>/vendor/jquery/jquery.min.js"></script>
     <script src="<?php echo URLROOT; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
