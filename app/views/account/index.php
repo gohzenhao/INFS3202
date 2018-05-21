9c62df2a6ca5ef118007e21ff9d81cb6153e8675
gohzenhao <gohzenhao@gmail.com>
2018-05-22 04:02:07 +1000

    Added side bar for account page.

--- app/views/account/index.php
+++ app/views/account/index.php
@@ -1,27 +1,40 @@
-<div class="container">
-  <div class="row">
-    <div class="col-lg-12">
+<div class="container-fluid">
+<div class="row content">
+    <div class="col-lg-2">
+
+    </div>
+    <div class="col-lg-2" id="sidebar">
+      <h3 class="mt-3 mb-4 pl-3"><u>Activities</u></h3>
+      <ul class="nav nav-pills nav-stacked">
+        <a href="#section1"><li><h5>My Recipes</h5></h5></li></a>
+        <a href="#section2"><li><h5>Create Recipe</h5></li></a>
+        <a href="#section1"><li><h5>Edit Profile</h5></li></a>
+      </ul><br>
+      <hr/>
+    </div>
+    <div class="col-lg-8">
       <div class="row mt-3">
-          <div class="col-lg-12 text-center">
-              <h3> <u> Your recipes </u> </h3>
+          <div class="col-lg-10 text-center">
+              <h3> <u> My Recipes </u> </h3>
           </div>
       </div>
 
-      <div class="row" style="overflow-x:scroll">
+      <div class="row">
 
           <?php foreach($data['recipes'] as $recipe){
             echo '
-              <div class="col-lg-3 m-3 text-center">
-                <div class="card" style="width: 18rem;">
-                  <img class="card-img-top" src="' . URLROOT.'/img'.$recipe['imagePath'] . '" alt="Card image cap">
+              <div class="col-lg-3 mt-3 mr-3 mb-3 text-center d-flex">
+                <div class="card" style="width: 25rem;">
+                  <img class="card-img-top" src="' . URLROOT.'/img'.$recipe['imagePath'] . '" alt="Card image cap" style="height:200px">
                   <div class="card-body">
                     <h5 class="card-title">' . $recipe['title'] . '</h5>
                     <p class="card-text">' . $recipe['description'] . '</p>
                   </div>
-                    <div class="card-body">
-                      <a href="#" class="card-link">edit recipe</a>
+                  <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><a href="#" class="card-link">edit recipe</a>
                       <a href="'.URLROOT.'/account/index?delete='. $recipe['rid'] .'" class="card-link">delete recipe</a>
-                    </div>
+                      </li>
+                  </ul>
                 </div>
               </div>
             ';
@@ -30,5 +43,9 @@
 
       </div>
     </div>
+    <div class="col-lg-1">
+
+    </div>
+
   </div>
 </div>
